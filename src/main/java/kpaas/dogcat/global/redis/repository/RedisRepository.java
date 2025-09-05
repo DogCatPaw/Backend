fe2d4b3e7bf44073719c508f734181f031cd4222
신윤진 <yunjin@sin-yunjin-ui-MacBookAir.local>
2025-09-05 14:50:40 +0900

    💚 cicd: 수정

--- src/main/java/kpaas/dogcat/global/redis/repository/RedisRepository.java
+++ src/main/java/kpaas/dogcat/global/redis/repository/RedisRepository.java
@@ -6,8 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RedisRepository extends CrudRepository<RedisRefreshToken, Long> {
-//    @NonNull
-//    Optional<RedisRefreshToken> findById(Long id);
-//    void deleteById(Long id);
-    //기본 제공됨
 }
