e0c51906035b47db38e0040abd47c896362023fb
신윤진 <yunjin@sin-yunjin-ui-MacBookAir.local>
2025-09-05 19:19:40 +0900

    fix: RedisConfig

--- src/main/java/kpaas/dogcat/global/redis/config/RedisConfig.java
+++ src/main/java/kpaas/dogcat/global/redis/config/RedisConfig.java
@@ -7,9 +7,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories(
+        basePackages = "kpaas.dogcat.global.redis.repository"
+)
 @RequiredArgsConstructor
 public class RedisConfig {
 
@@ -26,9 +30,9 @@ public class RedisConfig {
     // 스프링부트가 자동으로 RedisConnectionFactory 빈 등록해서 빈을 만들지 않아도됨
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         // redis와 통신할 템플릿 설정
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
 
         // key, value에 대한 직렬화 방법 설정
