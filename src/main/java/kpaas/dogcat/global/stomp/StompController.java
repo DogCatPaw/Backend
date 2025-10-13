4be844dc3c58e37adb5b14daa81ff88143f39f79
yunjin <yj007shin@gmail.com>
2025-10-13 18:32:36 +0900

    🐛 fix: stomp chat -> global

--- src/main/java/kpaas/dogcat/global/stomp/StompController.java
+++ src/main/java/kpaas/dogcat/global/stomp/StompController.java
@@ -1,4 +1,4 @@
-package kpaas.dogcat.domain.chat.stomp;
+package kpaas.dogcat.global.stomp;
 
 import kpaas.dogcat.domain.chat.dto.ChatReqDTO;
 import kpaas.dogcat.domain.chat.service.command.ChatMessageCommandService;
