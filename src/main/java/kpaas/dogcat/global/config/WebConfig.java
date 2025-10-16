607f689065d493fe8f4e97232eee12a3ab7d7539
yunjin <yj007shin@gmail.com>
2025-10-17 00:35:23 +0900

    🐛 fix: 지갑주소 헤더 받기

--- src/main/java/kpaas/dogcat/global/config/WebConfig.java
+++ src/main/java/kpaas/dogcat/global/config/WebConfig.java
@@ -1,25 +1,30 @@
 package kpaas.dogcat.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kpaas.dogcat.global.auth.WalletAddressResolver;
 import kpaas.dogcat.global.converter.OctetStreamReadMsgConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
     private final ObjectMapper objectMapper;
+    private final WalletAddressResolver walletAddressResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080",
-                                "http://localhost:3000",
-                                "https://jiangxy.github.io")
+                                "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
@@ -27,13 +32,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600);
     }
 
-    @Autowired
-    public WebConfig(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new OctetStreamReadMsgConverter(objectMapper));
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(walletAddressResolver);
     }
 }
